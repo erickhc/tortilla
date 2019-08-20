fe2726c54b210885fbca8fbc3d7e87027b0484e3
Erick Hernandez Curiel <Erick.HernandezCuriel@mx.bosch.com>
2019-08-20 09:50:38 -0500

    Add compile from file

--- src/compiler.rs
+++ src/compiler.rs
@@ -2,8 +2,9 @@ use crate::contract::Contract;
 use crate::abi::*;
 use std::io::prelude::*;
 use std::process::{Command, Stdio};
+use std::fs::File;
 
-pub fn compile(contract: &str) -> std::io::Result<Vec<Contract>> {
+pub fn compile_str(contract: &str) -> std::io::Result<Vec<Contract>> {
     let abis = compile_abi(contract)?;
     let bins = compile_bin(contract)?;
 
@@ -14,6 +15,13 @@ pub fn compile(contract: &str) -> std::io::Result<Vec<Contract>> {
        .map(|((name, abi), (_, bin))| Contract::new(name, abi, bin))
        .collect()
    )
+}
+
+pub fn compile_file(file: &mut File) -> std::io::Result<Vec<Contract>> {
+    let mut contract = String::new();
+    file.read_to_string(&mut contract)?;
+
+    compile_str(&contract)
 }
 
 fn compile_abi(contract: &str) -> std::io::Result<Vec<(String, Vec<Abi>)>> {
@@ -115,40 +123,11 @@ fn call_compiler(contract: &str, args: &[&str]) -> std::io::Result<String> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::fs::File;
+    use std::io::{Write, SeekFrom};
 
-    #[test]
-    fn test_compile_simple_contract() {
-        let input = r#"
-pragma solidity ^0.5.0;
-
-contract Migrations {
-  address public owner;
-  uint public last_completed_migration;
-
-  modifier restricted() {
-    if (msg.sender == owner) _;
-  }
-
-  constructor() public {
-    owner = msg.sender;
-  }
-
-  function setCompleted(uint completed) public restricted {
-    last_completed_migration = completed;
-  }
-
-  function upgrade(address new_address) public restricted {
-    Migrations upgraded = Migrations(new_address);
-    upgraded.setCompleted(last_completed_migration);
-  }
-}
-        "#.trim();
-
-        let contracts = compile(input).expect("Error compiling contract");
-        assert_eq!(contracts.len(), 1);
-        let contract = &contracts[0];
+    fn cmp_migrations_contract(contract: &Contract) {
         assert_eq!(contract.name, "Migrations");
-
         let abi = vec![
             Abi::Function(Function {
                 constant: false,
@@ -219,5 +198,77 @@ contract Migrations {
         let output_bin = "608060405234801561001057600080fd5b50336000806101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908373ffffffffffffffffffffffffffffffffffffffff1602179055506102b7806100606000396000f3fe608060405234801561001057600080fd5b506004361061004c5760003560e01c80630900f01014610051578063445df0ac146100955780638da5cb5b146100b3578063fdacd576146100fd575b600080fd5b6100936004803603602081101561006757600080fd5b81019080803573ffffffffffffffffffffffffffffffffffffffff16906020019092919050505061012b565b005b61009d6101f7565b6040518082815260200191505060405180910390f35b6100bb6101fd565b604051808273ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200191505060405180910390f35b6101296004803603602081101561011357600080fd5b8101908080359060200190929190505050610222565b005b6000809054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff163373ffffffffffffffffffffffffffffffffffffffff1614156101f45760008190508073ffffffffffffffffffffffffffffffffffffffff1663fdacd5766001546040518263ffffffff1660e01b815260040180828152602001915050600060405180830381600087803b1580156101da57600080fd5b505af11580156101ee573d6000803e3d6000fd5b50505050505b50565b60015481565b6000809054906101000a900473ffffffffffffffffffffffffffffffffffffffff1681565b6000809054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673";
 
         assert!(contract.bin.starts_with(output_bin));
+    }
+
+    #[test]
+    fn test_compile_simple_contract() {
+        let input = r#"
+        pragma solidity ^0.5.0;
+
+        contract Migrations {
+          address public owner;
+          uint public last_completed_migration;
+
+          modifier restricted() {
+            if (msg.sender == owner) _;
+          }
+
+          constructor() public {
+            owner = msg.sender;
+          }
+
+          function setCompleted(uint completed) public restricted {
+            last_completed_migration = completed;
+          }
+
+          function upgrade(address new_address) public restricted {
+            Migrations upgraded = Migrations(new_address);
+            upgraded.setCompleted(last_completed_migration);
+          }
+        }
+        "#.trim();
+
+        let contracts = compile_str(input).expect("Error compiling contract");
+        assert_eq!(contracts.len(), 1);
+        let contract = &contracts[0];
+
+        cmp_migrations_contract(contract);
+    }
+
+    #[test]
+    fn test_compile_from_file() {
+        let mut tmpfile: File = tempfile::tempfile().unwrap();
+        write!(tmpfile, "{}", r#"
+            pragma solidity ^0.5.0;
+
+            contract Migrations {
+              address public owner;
+              uint public last_completed_migration;
+
+              modifier restricted() {
+                if (msg.sender == owner) _;
+              }
+
+              constructor() public {
+                owner = msg.sender;
+              }
+
+              function setCompleted(uint completed) public restricted {
+                last_completed_migration = completed;
+              }
+
+              function upgrade(address new_address) public restricted {
+                Migrations upgraded = Migrations(new_address);
+                upgraded.setCompleted(last_completed_migration);
+              }
+            }"#.trim()
+        ).unwrap();
+
+        tmpfile.seek(SeekFrom::Start(0)).unwrap();
+        let contracts = compile_file(&mut tmpfile)
+            .expect("Couldn't compile contract from file");
+        assert_eq!(contracts.len(), 1);
+        let contract = &contracts[0];
+        cmp_migrations_contract(contract);
     }
 }
